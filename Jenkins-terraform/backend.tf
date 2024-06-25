d837666ca284cbbd7479bbe9c4d6d5b82644303b
Rahulk218 <gowdarahul1819@gmail.com>
2024-06-25 22:10:18 +0530

    provisining instance

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "tetris-tetris" 
     key    = "Jenkins/terraform.tfstate"
-    region = "ap-southeast-2"
+    region = "ap-south-1"
   }
 }
